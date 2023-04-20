a8ada45cb3b6a48bca9df0072198f087f934f127
Miguel Piedrafita <github@miguel.build>
2023-04-20 21:35:33 +0200

    Switch to workspace

--- packages/world-id-marbles/src/lib.rs
+++ packages/world-id-marbles/src/lib.rs
@@ -1,13 +1,14 @@
-#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use anyhow::Result;
 use indoc::formatdoc;
 use resvg::{
-    tiny_skia::{Pixmap, Transform},
+    tiny_skia::{Color, Pixmap, Transform},
     usvg::{self, TreeParsing},
 };
-use std::{fs, path::Path};
-use zkp_u256::U256;
+use std::{fmt::Debug, fs, path::Path};
+
+pub use ruint::aliases::U256;
 
 const COLORS: [&str; 36] = [
     "#FF0000", "#FF2B00", "#FF5500", "#FF8000", "#FFAA00", "#FFD500", "#FFFF00", "#D4FF00",
@@ -17,55 +18,99 @@ const COLORS: [&str; 36] = [
     "#FF00D5", "#FF00AA", "#FF0080", "#FF0055",
 ];
 
+pub trait Seedable {
+    fn to_seed(self) -> U256;
+}
+
+impl Seedable for u32 {
+    fn to_seed(self) -> U256 {
+        U256::try_from(self).unwrap()
+    }
+}
+impl Seedable for U256 {
+    fn to_seed(self) -> U256 {
+        self
+    }
+}
+
+impl Seedable for usize {
+    fn to_seed(self) -> U256 {
+        U256::try_from(self).unwrap()
+    }
+}
+
+impl Seedable for String {
+    fn to_seed(self) -> U256 {
+        U256::from_str_radix(&self, 10).unwrap()
+    }
+}
+impl Seedable for &str {
+    fn to_seed(self) -> U256 {
+        U256::from_str_radix(self, 10).unwrap()
+    }
+}
+
 pub struct Marble {
     seed: U256,
 }
 
 impl Marble {
     /// Create a new marble with the given seed.
-    pub fn new<T>(seed: T) -> Self
-    where
-        U256: From<T>,
-    {
+    ///
+    /// # Panics
+    ///
+    /// Panics if the seed cannot be converted to a `U256`.
+    pub fn new(seed: impl Seedable) -> Self {
         Self {
-            seed: U256::from(seed),
+            seed: seed.to_seed(),
         }
     }
 
-    fn random_number<T>(&mut self, max: T) -> u32
+    fn random_number<T, E>(&mut self, max: T) -> T
     where
-        U256: From<T>,
+        E: Debug,
+        T: Seedable + TryFrom<U256, Error = E> + Copy,
     {
-        let max = U256::from(max);
+        let max = max.to_seed();
 
-        let result = self.seed.clone() % max.clone();
+        let result = self.seed % max;
         self.seed /= max;
 
-        result.as_u32()
+        result.try_into().unwrap()
+    }
+
+    fn random_sort<T>(&mut self, mut arr: Vec<T>) -> Vec<T> {
+        let mut shuffled = vec![];
+
+        while !arr.is_empty() {
+            let k = self.random_number(arr.len());
+
+            shuffled.push(arr.remove(k));
+        }
+
+        shuffled
     }
 
     fn random_color(&mut self) -> &str {
-        let index = self.random_number(COLORS.len()) as usize;
-
-        COLORS[index]
+        COLORS[self.random_number(COLORS.len())]
     }
 
     /// Build the SVG for the marble.
     #[must_use]
     pub fn build_svg(&mut self) -> String {
-        let mut shapes = vec![
+        let shapes = vec![
             formatdoc!(
                 r#"
-                <g filter="url(#blur)" opacity=".8">
-                    <path fill="{color}" d="M78.824-16.686c17.78 14.541 4.24 87.76-2.637 82.948-4.194-2.935-9.153-27.765-22.32-38.405-8.418-6.802-23.488-1.839-33.086-1.137-24.614 1.8 40.115-58.069 58.043-43.406Z"/>
+                <g filter="url(#blur)" opacity=".9">
+                    <ellipse cx="33.545" cy="32.494" fill="{color}" rx="33.545" ry="32.494" transform="matrix(-.48289 -.87568 .7985 -.602 9.46 74.034)"/>
                 </g>
             "#,
                 color = self.random_color()
             ),
             formatdoc!(
                 r#"
-                <g filter="url(#blur)" opacity=".9">
-                    <ellipse cx="33.545" cy="32.494" fill="{color}" rx="33.545" ry="32.494" transform="matrix(-.48289 -.87568 .7985 -.602 9.46 74.034)"/>
+                <g filter="url(#blur)" opacity=".8">
+                    <path fill="{color}" d="M78.824-16.686c17.78 14.541 4.24 87.76-2.637 82.948-4.194-2.935-9.153-27.765-22.32-38.405-8.418-6.802-23.488-1.839-33.086-1.137-24.614 1.8 40.115-58.069 58.043-43.406Z"/>
                 </g>
             "#,
                 color = self.random_color()
@@ -79,8 +124,6 @@ impl Marble {
                 color = self.random_color()
             ),
         ];
-
-        shapes.sort_by(|_, _| self.random_number(2).cmp(&1));
 
         formatdoc!(
             r##"
@@ -99,8 +142,8 @@ impl Marble {
                     </defs>
                 </svg>
         "##,
-            rotation = self.random_number(359),
-            shapes = shapes.join("")
+            shapes = self.random_sort(shapes).join(""),
+            rotation = self.random_number(359_u32),
         )
     }
 
@@ -111,17 +154,18 @@ impl Marble {
     ///
     /// This function will return an error if the marble cannot be rendered.
     /// This can happen if the SVG fails to be parsed or the PNG cannot be encoded.
-    pub fn render_png(&mut self, width: u32, height: u32) -> Result<Vec<u8>> {
+    pub fn render_png(&mut self, size: u32) -> Result<Vec<u8>> {
         let svg = self.build_svg();
         let tree = usvg::Tree::from_data(svg.as_bytes(), &usvg::Options::default())?;
 
-        let mut pixmap = Pixmap::new(width, height).ok_or_else(|| {
-            anyhow::anyhow!("Failed to create pixmap with size {}x{}", width, height)
+        let mut pixmap = Pixmap::new(size, size).ok_or_else(|| {
+            anyhow::anyhow!("Failed to create pixmap with size {}x{}", size, size)
         })?;
+        pixmap.fill(Color::TRANSPARENT);
 
         resvg::render(
             &tree,
-            resvg::FitTo::Size(width, height),
+            resvg::FitTo::Width(size),
             Transform::default(),
             pixmap.as_mut(),
         )
@@ -135,8 +179,8 @@ impl Marble {
     /// # Errors
     ///
     /// This function will return an error if the marble cannot be rendered or saved.
-    pub fn save_png<P: AsRef<Path>>(&mut self, width: u32, height: u32, path: P) -> Result<()> {
-        fs::write(path, self.render_png(width, height)?)?;
+    pub fn save_png<P: AsRef<Path>>(&mut self, size: u32, path: P) -> Result<()> {
+        fs::write(path, self.render_png(size)?)?;
 
         Ok(())
     }
